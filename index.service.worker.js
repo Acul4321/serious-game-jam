50a72de9d86ff56f9c5da28f207f0064a80ff200
Acul4321 <Acul4321@users.noreply.github.com>
2026-06-23 15:21:12 +0000

    Deploying to gh-pages from @ Acul4321/serious-game-jam@c7471abc5db3730e0badd0efdc2e31871b2b0807 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1782226552|3216126';
+const CACHE_VERSION = '1782228062|3143351';
 /** @type {string} */
 const CACHE_PREFIX = 'serious-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
